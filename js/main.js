f82ffe51cec10a732597b71f3ef1b83c5bd28ebb
Luciano <lferraz.dev@gmail.com>
2020-10-13 18:32:46 -0300

    Init project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var numberElement = document.querySelector('#number');
+var msgElement = document.querySelector('#msg');
+var buttonElement = document.querySelector('#send');
+
+buttonElement.onclick = function() {
+    var number = numberElement.value;
+    var msg = msgElement.value;
+    buttonElement.setAttribute('href', `https://api.whatsapp.com/send?phone=+55${number}&text=${msg}`);
+}
